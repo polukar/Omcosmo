5755c0a44c4327f8e63f4180f9e3a0c019bee228
yevgeniykonovalov <zhosakic@gmail.com>
2019-11-17 21:28:12 +0600

    Сделал аккардеон.

--- src/js/main.js
+++
@@ -1,3 +0,0 @@
-$(window).load(function() {
-    $("#before-after").twentytwenty();
-});
--- src/js/template/_main.js
+++ src/js/template/_main.js
@@ -1,1 +1,12 @@
+let acc = document.getElementsByClassName('question');
+let i;
+    for (i=0; i < acc.length; i++)
+        acc[i].onclick = function () {
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('invisible');
+        };
 
+
+$(window).load(function() {
+    $("#before-after").twentytwenty();
+});
