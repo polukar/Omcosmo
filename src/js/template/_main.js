959bcbed107915ba4d6117fe450ec8037666fd21
yevgeniykonovalov <zhosakic@gmail.com>
2019-11-21 18:39:17 +0600

    Тестовая страница

--- src/js/template/_main.js
+++ src/js/template/_main.js
@@ -6,7 +6,15 @@ let i;
             this.nextElementSibling.classList.toggle('invisible');
         };
 
+let accPrice = document.getElementsByClassName('price-list__headline');
+let n;
+for (n=0; n < accPrice.length; n++)
+    accPrice[n].onclick = function () {
+        console.log('123');
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('invisible');
+    };
 
-$(window).load(function() {
-    $("#before-after").twentytwenty();
-});
+// $(window).load(function() {
+//     $("#before-after").twentytwenty();
+// });
